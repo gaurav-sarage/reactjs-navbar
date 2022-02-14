ef02da81b17899af1bb91368f46f51ca0801aa11
Gaurav <gauravsarage.1902@gmail.com>
2022-02-14 16:11:18 +0530

    initialisze project

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Navbar.css'
+
+export default function Navbar() {
+    return ( <
+        nav >
+        <
+        ul className = 'list' >
+        <
+        li className = 'items' > Home < /li> <
+        li className = 'items' > About < /li> <
+        li className = 'items' > Services < /li> <
+        li className = 'items' > Pricing < /li> <
+        li className = 'items' > Contact < /li> <
+        /ul> <
+        button className = "btn" > Click Here < /button> <
+        /nav>
+    )
+}
